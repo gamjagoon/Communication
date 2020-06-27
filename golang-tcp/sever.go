9cc93ff39341a61c745321f84c23dfb6e040fcee
gamjagoon <rlaalswo201@gmail.com>
2020-06-27 17:17:12 +0900

    update data

--- golang-tcp/sever.go
+++ golang-tcp/sever.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
+// MssqlConnect is type  
+type MssqlConnect struct {
+	Database string `json:"database"`
+	User struct {
+		ID  string `json:"id"`
+		Pwd string `json:"pwd"`
+	} `json:"user"`
+	Host struct {
+		Address string `json:"address"`
+		Port    string    `json:"port"`
+	} `json:"host"`
+}
+
+// Park datastruct
+type Park struct {
+	Name string `json:"name"`
+	Total int `json:"total"`
+	Empty int `json:"empty"`
+}
+
 
 func errHandler(err error) {
 	if err != nil {
@@ -13,15 +37,24 @@ func errHandler(err error) {
 	}
 }
 
+var (
+	db *sql.DB
+)
+
+
 func main() {
+	config , err := LoadConfig()
+	errHandler(err)
 
 	// sever open
-	l, err := net.Listen("tcp", ":30122")
-	if nil != err {
-		fmt.Print("fial to bind address", err)
-		return
-	}
+	l, err := net.Listen("tcp",config.Host.Port)
+	errHandler(err)
 	defer l.Close()
+
+	dbname := config.User.ID+":"+config.User.Pwd+"@/"+"park"
+	db, err = sql.Open("mysql",dbname) 
+	errHandler(err)
+	defer db.Close()
 
 	// loop server
 	for {
@@ -37,7 +70,6 @@ func main() {
 // ConnHandler input db receved data
 func ConnHandler(conn net.Conn) {
 	recvBuf := make([]byte, 256)
-	ok := []byte{'o', 'k'}
 	for {
 		n, err := conn.Read(recvBuf)
 		if nil != err {
@@ -49,17 +81,27 @@ func ConnHandler(conn net.Conn) {
 			return
 		}
 		if 0 < n {
-			// pm10 pm25 hum tem time
 			recv := recvBuf[:n]
 			data := string(recv)
-			fmt.Println(data)
-			conn.Write(ok)
-			datas := strings.Split(data, " ")
-			Time := datas[4]+ " " + datas[5]
-			_, err := db.Exec("insert into data(pm10,pm25,time) values (?,?,?)",datas[0], datas[1], Time)
+			fmt.Println("receve = ",data)
+			onepark := Park{}
+			err = db.QueryRow("select juso,total,empty from park_id where x = $1 and y = $2",37.575929,126.976849).Scan(&onepark.Name,&onepark.Total,&onepark.Empty)
 			errHandler(err)
-			_, err =db2.Exec("insert into hum(hum,tem,time) values (?,?,?)",datas[2], datas[3], Time)
-			errHandler(err)
+			sendbyte, _ := json.Marshal(onepark)
+			conn.Write(sendbyte)
+			fmt.Printf("%s total = %d, empty = %d",onepark.Name,onepark.Total,onepark.Empty)
 		}
 	}
+}
+
+// LoadConfig is retturn config json
+func LoadConfig() (MssqlConnect, error){
+	var config MssqlConnect
+	file , err := os.Open("config.json")
+	defer file.Close()
+	errHandler(err)
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	errHandler(err)
+	return config, err
 }
